6367b6a2377f74d44baafc7a817c4a36d8fbf2e4
evanildo <evanildo.sf91@gmail.com>
2023-12-13 22:08:33 -0300

    feat: signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import logoImg from "../../../public/logo-pizzaria.png";
+import SignupForm from "../components/SignupForm";
+
+export default function Signup() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Image src={logoImg} alt={"Logo project pizzaria"} />
+      <div className="w-[37.5rem] sm:w-[90%] mt-8 flex flex-col items-center justify-center py-8 px-6">
+        <SignupForm />
+        <Link className="mt-4 hover:cursor-pointer" href={"/"}>
+          Já possuo uma conta
+        </Link>
+      </div>
+    </main>
+  );
+}
