1451ed2687c8a53233328fded794ad1f55e0c78e
evanildo <evanildo.sf91@gmail.com>
2023-12-16 16:14:29 -0300

    feat: add signup function

--- src/components/SignupForm/index.tsx
+++ src/components/SignupForm/index.tsx
@@ -1,32 +1,44 @@
 "use client";
 
+import { fetchWrapper } from "@/services/api";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
 
 export default function SignupForm() {
   const signupForm = useRef(null);
+  const router = useRouter();
 
   async function handleOnSubmit(formData: FormData) {
+    debugger;
     const name = formData.get("name");
     const email = formData.get("email");
     const password = formData.get("password");
 
     if (!name || !email || !password) {
       // TODO adicionar toast para informar erro de preenchimento do formulário.
-      console.log("🚀 ~ Todos os campos são obrigatórios");
       return;
     }
 
-    // TODO lógica para realizar o login do usuário.
-    console.log(
-      "🚀 ~ file: index.tsx:12 ~ handleOnSubmit ~ email ~ password:",
-      name,
-      " - ",
-      email,
-      " - ",
-      password
-    );
+    await fetchWrapper("users/signup", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+      .then((res) => {
+        // TODO adicionar toast para informar sucesso no cadastro de usuário.
+        router.replace("/");
+      })
+      .catch((error) => {
+        // TODO adicionar toast para informar erro no cadastro de usuário.
+      });
   }
   return (
     <form
