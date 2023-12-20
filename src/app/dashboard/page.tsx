763dd21e61d5b3f1e8ee00a6d96f57a5023b6f5d
evanildo <evanildo.sf91@gmail.com>
2023-12-19 22:18:53 -0300

    feat: dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardDisplay from "@/components/DashboardDisplay";
+import Header from "@/components/Hearder";
+
+export default async function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <DashboardDisplay />
+    </div>
+  );
+}
