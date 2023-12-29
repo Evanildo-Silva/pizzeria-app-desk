8bfa780dcacdf3510b091eeef708dc1eba7cac2e
evanildo <evanildo.sf91@gmail.com>
2023-12-29 15:03:44 -0300

    refactor: add the priority property to the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import logoImg from "../../public/logo-pizzaria.png";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Image src={logoImg} alt={"Logo project pizzaria"} />
+      <Image priority={true} src={logoImg} alt={"Logo project pizzaria"} />
       <div className="w-[37.5rem] sm:w-[90%] mt-8 flex flex-col items-center justify-center py-8 px-6">
         <LoginForm />
         <Link className="mt-4 hover:cursor-pointer" href={"/signup"}>
