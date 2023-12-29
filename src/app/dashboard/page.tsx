6f10e8d54aa7e68fe1426c28e91ac45119ea041c
evanildo <evanildo.sf91@gmail.com>
2023-12-29 14:54:23 -0300

    feat: page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import DashboardDisplay from "@/components/DashboardDisplay";
 import Header from "@/components/Hearder";
+import { getOrders } from "@/lib/actions/orders";
 
 export default async function Dashboard() {
+  const orders: any = await getOrders();
   return (
     <div>
       <Header />
-      <DashboardDisplay />
+      <DashboardDisplay orderList={orders} />
     </div>
   );
 }
